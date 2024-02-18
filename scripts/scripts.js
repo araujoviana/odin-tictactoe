3c6f235beb9d20dfb4de7fc8e4e2f1a9a2c71f33
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-02-18 19:23:48 -0300

    Add todo list

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,29 @@
+// TODO list after brainstorming a bit
+// 1. Create a 3x3 grid
+// 2. Assign an empty value to the parts of the grid that don't have a value
+// 3. Assign symbols for each player object X and O
+// 4. Create a list of player objects
+// 5. Make a loop for each player's turn
+// 6. Player inserts index in the console
+// 7. Check if the index is already inserted
+// 8. If yes, ask to insert again
+// 9. Their respective symbol is inserted in the grid
+// 10. Check if any victory condition has been met
+// 11. If not, continue the game
+// 12. If yes, find out which symbol made the victory
+// 13. Announce who won
+
 const gameboard = (() => {
-    const gameboardArray = [];
-    return { gameboardArray }
+    const gameboardArray = [['','',''], ['','',''], ['','','']];
+    return { gameboardArray };
 })();
 
-function createPlayer() {
+// Placeholder until i understand what this is supposed to do
+const gameFlow = (() => {
+    const playerList = [];
+    return {  };
+})();
 
+function createPlayer(name) {
+    return { name };
 }
