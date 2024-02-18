5d42031ea6d02e4e97973896398a2110b2020bb5
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-02-18 20:07:28 -0300

    Add check for empty space

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,8 +3,8 @@
 // 2. Assign an empty value to the parts of the grid that don't have a value - done
 // 3. Assign symbols for each player object X and O - done
 // 4. Create a list of player objects - done
-// 5. Make a loop for each player's turn
-// 6. Player inserts index in the console
+// 5. Make a loop for each player's turn - done
+// 6. Player inserts index in the console - done
 // 7. Check if the index is already inserted
 // 8. If yes, ask to insert again
 // 9. Their respective symbol is inserted in the grid
@@ -15,28 +15,46 @@
 
 let xIsUsed = false;
 
-const gameboard = (() => {
-    const gameboardArray = [['','',''], ['','',''], ['','','']];
-    return { gameboardArray };
+const gameBoard = (() => {
+    const gameboardArray = ['','','','','','','','',''];
+
+    const checkEmptySpace = (index) => {
+        return gameboardArray[index] == '';
+    }    
+
+    return { gameboardArray, checkEmptySpace };
 })();
 
 function createPlayer(symbol) {
     const getTurn = () => {
-        return window.prompt("TICTACTOE INDEX: ");
+        return Number(window.prompt("TICTACTOE INDEX: "));
     }
-    return { symbol };
+    return { symbol, getTurn };
 }
 
-// Placeholder until i understand what this is supposed to do
 const gameFlow = (() => {
     const playerList = [createPlayer('X'), createPlayer('O')];
+
     let gameOver = false;
-    const gameLoop = () => {
+
+    // This is probably unoptimized
+    const startGameLoop = () => {
+        
         while (!gameOver) {
             playerList.forEach(player => {
+
+                let playerChoice;
+                do {
+                    playerChoice = player.getTurn();
+                }
+                while (gameBoard.checkEmptySpace(playerChoice));
+
+                gameBoard.gameboardArray[playerChoice] = player.symbol;
                 
+                // Check here for a win condition
             });
         }
     };
-    return { playerList, };
+
+    return { playerList, startGameLoop };
 })();
