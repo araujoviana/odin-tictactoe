762157e3803e0782de8d9e741d3aa0528268fb49
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-02-20 19:33:42 -0300

    Fix click event register

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,54 +1,49 @@
-// Build the grid on screen DONE
-// Clean up this mess
-// Change get turn for click instead of alert prompts - DONE
-// On click on a grid element, place it based on its index
-// Remove old alert stuff
-
 const gameBoard = (() => {
-    // TEMPORARY
-    //const gameboardArray = ['X','X','X','X','X','X','X','X','X'];
-    const gameboardArray = ['','','','','','','','',''];
+    const gameboardArray = ['', '', '', '', '', '', '', '', ''];
 
     const checkEmptySpace = (index) => {
-        return gameboardArray[index] == '';
-    }    
+        return gameboardArray[index] === '';
+    }
 
     const checkWinCondition = () => {
-        let winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
 
-        const CheckEqualSymbol = (x, y, z) => {
-            return gameboardArray[x] == gameboardArray[y] && gameboardArray[y] == gameboardArray[z] && gameboardArray[x] != '';
+        const checkEqualSymbol = (x, y, z) => {
+            return gameboardArray[x] === gameboardArray[y] && gameboardArray[y] === gameboardArray[z] && gameboardArray[x] !== '';
         }
-        
-        for(let i = 0; i < winningCombinations.length; i++) {
-            let [x, y, z] = winningCombinations[i];
-            if(CheckEqualSymbol(x, y, z)) {
+
+        for (const combination of winningCombinations) {
+            const [x, y, z] = combination;
+            if (checkEqualSymbol(x, y, z)) {
                 return [true, gameboardArray[x]];
             }
         }
         return [false, null];
-
     };
-   
+
     return { gameboardArray, checkEmptySpace, checkWinCondition };
 })();
 
 function createPlayer(symbol) {
     const getTurn = (callback) => {
         let clickableSquares = document.querySelectorAll(".tictactoe-square");
-    
+
         clickableSquares.forEach((square, index) => {
             square.addEventListener('click', () => {
+                console.log("CLICKED");
                 callback(index);
             });
         });
-    }    
+    }
     return { symbol, getTurn };
 }
 
 const gameFlow = (() => {
     const playerList = [createPlayer('X'), createPlayer('O')];
-
     let gameOver = false;
 
     const displayBoardArray = () => {
@@ -63,21 +58,21 @@ const gameFlow = (() => {
         });
     }
 
-    // Changed to recursive using callback instead of whatever i was doing
     const startGameLoop = () => {
         let winnerSymbol;
         let currentPlayerIndex = 0;
-    
+        displayBoardArray();
+
         const playTurn = () => {
             let player = gameFlow.playerList[currentPlayerIndex];
-    
-            getTurn((index) => {
-                if (gameBoard.checkEmptySpace(index)) {
+
+            player.getTurn((index) => {
+                if (gameBoard.checkEmptySpace(index) && !gameOver) {
                     gameBoard.gameboardArray[index] = player.symbol;
                     displayBoardArray();
-    
+
                     [gameOver, winnerSymbol] = gameBoard.checkWinCondition();
-    
+
                     if (gameOver || gameBoard.gameboardArray.every(cell => cell !== '')) {
                         if (winnerSymbol) {
                             console.log(`${winnerSymbol} WINS!`);
@@ -91,8 +86,8 @@ const gameFlow = (() => {
                 }
             });
         };
-    
-        playTurn(); // Start the first turn
+
+        playTurn();
     };
 
     return { playerList, startGameLoop, displayBoardArray };
